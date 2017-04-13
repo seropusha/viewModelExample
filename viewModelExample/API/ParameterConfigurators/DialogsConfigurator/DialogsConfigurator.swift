adcdf943213136662318c7fe44ec70786dedca83
seropusha <seropywa@gmail.com>
2017-04-13 11:28:48 +0300

    add configurator protocol / parametersBuilders

---
+++ viewModelExample/API/ParameterConfigurators/DialogsConfigurator/DialogsConfigurator.swift
@@ -0,0 +1,17 @@
+//
+//  DialogsConfigurator.swift
+//  viewModelExample
+//
+//  Created by Valeriya Ponamarenko on 4/12/17.
+//  Copyright © 2017 Navka Sergey. All rights reserved.
+//
+
+import Foundation
+
+struct DialogsConfigurator: ParameterConfiguratorProtocol {
+    //create properties
+    func configureParameters(builder: ParametersBuilder) {
+        //to do
+    }
+    
+}
---
+++ viewModelExample/API/ParameterConfigurators/ParameterConfiguratorProtocol.swift
@@ -0,0 +1,13 @@
+//
+//  ParameterConfiguratorProtocol.swift
+//  viewModelExample
+//
+//  Created by Valeriya Ponamarenko on 4/12/17.
+//  Copyright © 2017 Navka Sergey. All rights reserved.
+//
+
+import Foundation
+
+protocol ParameterConfiguratorProtocol {
+    func configureParameters(builder: ParametersBuilder)
+}
