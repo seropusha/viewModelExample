eb570a9908f5e9cf9391fcf5d277372bf2a095b4
seropusha <seropywa@gmail.com>
2017-04-13 11:29:15 +0300

    fix

---
+++ viewModelExample/API/ParametersBuilder/ParametersBuilder.swift
@@ -0,0 +1,13 @@
+//
+//  ParameterBuilder.swift
+//  viewModelExample
+//
+//  Created by Valeriya Ponamarenko on 4/12/17.
+//  Copyright © 2017 Navka Sergey. All rights reserved.
+//
+
+import Foundation
+
+struct ParametersBuilder {
+    
+}
